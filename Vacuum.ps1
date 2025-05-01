418e158bef726641e599245bd13d7e92972609ca
icuffem <an.anonymous.coffee@gmail.com>
2025-05-01 18:47:11 -0400

    Update

--- Vacuum.ps1
+++ Vacuum.ps1
@@ -1,33 +1,36 @@
 param (
 
-    $filePath
+    [string] $filePath
 )
 
 $fileContent = Get-Content -Path $filePath -Raw
 
 foreach ($line in $fileContent -Split "`n") {
 
-    $prompt = $line -split ";" | ForEach-Object { $_.Trim() }
+    if (-Not [String]::IsNullOrEmpty($line)) {
 
-    switch ($prompt[0]) {
-
-        "DEVELOPER" {
-
-            Write-Output $prompt[1]
-        }
-
-        "DISABLE" {
-
-            Write-Output $prompt[1]
-
-            adb.exe shell "pm disable --user 0 $($prompt[2])"
-        }
-
-        "UNINSTALL" {
-
-            Write-Output $prompt[1]
-
-            adb.exe shell "pm uninstall --user 0 $($prompt[2])"
+        $prompt = $line.Trim() -Split ";"
+    
+        switch ($prompt[0]) {
+    
+            "DEVELOPER" {
+    
+                Write-Output $prompt[1]
+            }
+    
+            "DISABLE" {
+    
+                Write-Output $prompt[1]
+    
+                adb.exe shell "pm disable --user 0 $($prompt[2])"
+            }
+    
+            "UNINSTALL" {
+    
+                Write-Output $prompt[1]
+    
+                adb.exe shell "pm uninstall --user 0 $($prompt[2])"
+            }
         }
     }
 }
