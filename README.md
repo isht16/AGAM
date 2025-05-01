28c37a3c352fa2ef061f47c78a900ebd03caedc6
icuffem <an.anonymous.coffee@gmail.com>
2025-05-01 04:54:16 -0400

    Update

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 _I recommend cleaning the OS after an update._
 
+https://developer.android.com/tools/releases/platform-tools
+
 ## Application Management Instructions (AMI) format
 
 I have created a small format to have a basis for reusing code in other terminals.
--- Vacuum.ps1
+++ Vacuum.ps1
@@ -1,38 +1,38 @@
 param (
 
-    [string] $filePath
+    $filePath
 )
 
 $fileContent = Get-Content -Path $filePath -Raw
 
 foreach ($line in $fileContent -Split "`n") {
 
-    $prompt = $line -Split ";"
+    $prompt = $line -split ";" | ForEach-Object { $_.Trim() }
 
     switch ($prompt[0]) {
 
         "SEC" {
 
-            Write-Output "--- $($prompt[1]) ---"
+            Write-Output "$($prompt[1])"
         }
 
         "ENA" {
 
+            .\adb.exe shell "pm default-state --user 0 $($prompt[1])"
         }
 
         "DIS" {
 
+            .\adb.exe shell "pm disable-user --user 0 $($prompt[1])"
         }
 
-        "ADD" {
-
-        }
+        # "ADD" {
+        #
+        # }
 
         "REM" {
 
-            Write-Output "pm uninstall --user 0 $($prompt[1])"
-
-            # .\adb.exe shell "pm uninstall --user 0 $($prompt[1])"
+            .\adb.exe shell "pm uninstall --user 0 $($prompt[1])"
         }
     }
 }
